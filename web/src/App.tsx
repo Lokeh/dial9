4405e4c6b7d7da6a7d5c6e9af03a88a8690482f3
Will <will@willacton.com>
2017-02-17 13:33:43 -0800

    react-bootstrap

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,19 +1,26 @@
 import * as React from 'react';
 import './App.css';
-
-const logo = require('./logo.svg');
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import {
+  Grid,
+  Row,
+  Col,
+  PageHeader,
+} from 'react-bootstrap';
 
 class App extends React.Component<null, null> {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <div>
+        <Grid>
+          <Row><Col><PageHeader style={{textAlign: "center"}}>Dial9</PageHeader></Col></Row>
+          <Row>
+            <Col>
+              
+            </Col>
+          </Row>
+        </Grid>
       </div>
     );
   }
